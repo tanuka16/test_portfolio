787a45bf834c2001b7a0a40a2da19e52455b66ed
Tanuka16 <das.tanuka@yahoo.com>
2019-09-26 17:19:59 -0400

    updated

---
+++ build/precache-manifest.fd8e9fe5f78e1a010698139a175d5a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e9adacac51cd863f1956f08fc8134a",
+    "url": "/tanukaDas_portfolio/index.html"
+  },
+  {
+    "revision": "74262395ad9c02800727",
+    "url": "/tanukaDas_portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "9266200e90387e96e4b8",
+    "url": "/tanukaDas_portfolio/static/css/main.33f861f0.chunk.css"
+  },
+  {
+    "revision": "74262395ad9c02800727",
+    "url": "/tanukaDas_portfolio/static/js/2.b5b7fe29.chunk.js"
+  },
+  {
+    "revision": "9266200e90387e96e4b8",
+    "url": "/tanukaDas_portfolio/static/js/main.c1afee2c.chunk.js"
+  },
+  {
+    "revision": "4d54aef6b32162d424d8",
+    "url": "/tanukaDas_portfolio/static/js/runtime~main.832e2d6b.js"
+  }
+]);
