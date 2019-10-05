2b7536d01093096b353edb78e9b273592af9eda0
Tanuka16 <das.tanuka@yahoo.com>
2019-10-04 23:49:41 -0400

    updated resume

---
+++ build/precache-manifest.6461ad1cb050a8e3cd1aa2ca2b6687c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbfb132b86242352c38b849a098b28a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14cddc073ea039321526",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "96f7b1d914dc1aa6b438",
+    "url": "/static/css/main.33f861f0.chunk.css"
+  },
+  {
+    "revision": "14cddc073ea039321526",
+    "url": "/static/js/2.0422ff38.chunk.js"
+  },
+  {
+    "revision": "96f7b1d914dc1aa6b438",
+    "url": "/static/js/main.527fd834.chunk.js"
+  },
+  {
+    "revision": "3216fb0461cea5630854",
+    "url": "/static/js/runtime~main.918d1130.js"
+  }
+]);
