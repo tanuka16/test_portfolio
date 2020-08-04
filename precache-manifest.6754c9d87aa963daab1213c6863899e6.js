5c74bfbd7e598489708003830feb44fcd5dd99aa
Tanuka16 <tanuka.das12@gmail.com>
2020-08-04 01:53:46 -0400

    Updates

---
+++ precache-manifest.6754c9d87aa963daab1213c6863899e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd91b19e4d64d6444841c7d8b7a446f",
+    "url": "/tanukaDasPortfolio/index.html"
+  },
+  {
+    "revision": "54b437155e715d92929b",
+    "url": "/tanukaDasPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "eb75a830b1a274e3d491",
+    "url": "/tanukaDasPortfolio/static/css/main.4f64ccd5.chunk.css"
+  },
+  {
+    "revision": "54b437155e715d92929b",
+    "url": "/tanukaDasPortfolio/static/js/2.b21f9575.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/tanukaDasPortfolio/static/js/2.b21f9575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb75a830b1a274e3d491",
+    "url": "/tanukaDasPortfolio/static/js/main.f7d06eb8.chunk.js"
+  },
+  {
+    "revision": "4ebface0ae6ebfb85f8a",
+    "url": "/tanukaDasPortfolio/static/js/runtime-main.4a1cdddb.js"
+  }
+]);
